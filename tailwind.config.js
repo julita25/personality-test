2af576d4251555c46453a40e61ff816bf0c3cdc4
juli <julitati25@gmail.com>
2022-12-19 11:04:50 +0400

    add tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+],
+theme: {
+  colors: {
+    transparent: "transparent",
+    current: "currentColor",
+    black: colors.black,
+    white: colors.white,
+    gray: colors.gray,
+    indigo: colors.indigo,
+    red: colors.rose,
+    yellow: colors.amber,
+    blue: colors.blue,
+    green: colors.green,
+    orange: colors.orange
   },
-  plugins: [],
+  fontFamily: {
+    title: "var(--font-title)",
+    default: "var(--font-default)",
+    small: "var(--font-small)"
+  },
+  extend: {}
+},
+  plugins: []
 }
