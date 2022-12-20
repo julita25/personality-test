f23f1179a4a95a5b01e0ee79938b0a122ac30744
juli <julitati25@gmail.com>
2022-12-20 15:49:44 +0400

    add progress dots

--- components/TestSteps/TestSteps.jsx
+++ components/TestSteps/TestSteps.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-const TestSteps = ({ steps, currentIndex }) => {
-  console.log(currentIndex);
-  return (
-    <div className="h-full flex flex-col justify-between">
+const TestSteps = ({ steps, currentIndex }) => (
+  <div className="h-full flex flex-col justify-between">
+    {
+      steps[currentIndex]
+    }
+    <div className="flex space-x-5 justify-center my-8">
       {
-        steps[currentIndex]
+        steps.map((step, index) => (
+          <div
+            key={step.type?.name}
+            className={`w-4 h-4 rounded-full ${index === currentIndex ? "bg-gray-500" : "bg-gray-300"}`}
+          />
+        ))
       }
     </div>
-  );
-}
+  </div>
+);
+
 
 export default TestSteps;
