a08e192e974873be1474006502f80ea88f4a4b34
juli <julitati25@gmail.com>
2022-12-20 21:48:50 +0400

    add logic for completing test

--- components/PersonalityModule/PersonalityModule.jsx
+++ components/PersonalityModule/PersonalityModule.jsx
@@ -1,31 +1,44 @@
-import React, { useState } from 'react'
-import Step from '../Step/Step'
-import TestSteps from '../TestSteps/TestSteps'
-import testQuestions from "../../data/questions.json"
+import React, { useState } from "react";
+import Step from "../Step/Step";
+import TestSteps from "../TestSteps/TestSteps";
+import testQuestions from "../../data/questions.json";
+import { useRouter } from "next/router";
 
 const PersonalityModule = () => {
+  const router = useRouter();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [answers, setAnswers] = useState({});
 
-  const nextStep = () => setCurrentIndex(currentIndex + 1);
-  const prevStep = () => setCurrentIndex(currentIndex - 1);
+  const onNextStep = (answer) => {
+    setCurrentIndex(currentIndex + 1);
+    setAnswers(answer);
+  };
+
+  const onComplete = (answer) => {
+    setAnswers(answer);
+    const score = Object.values(answers).reduce((prev, current) => prev + current, 0);
+    console.log(score);
+    router.push(`/results/${score}`);
+  };
+
+  const onPrevStep = () => setCurrentIndex(currentIndex - 1);
 
   const getSteps = () => (
     testQuestions.map((it, index) => (
       <Step
-        title={it.question}
-        options={it.options}
-        next={index === testQuestions.length - 1 ? null : nextStep}
-        prev={index === 0 ? null : prevStep}
+        item={it}
+        next={index === testQuestions.length - 1 ? onComplete : onNextStep}
+        prev={index !== 0 && onPrevStep}
+        selectedAnswers={answers}
       />
-    )
-    )
-  )
+    ))
+  );
 
   return (
     <>
       <TestSteps steps={getSteps()} currentIndex={currentIndex} />
     </>
   )
-}
+};
 
-export default PersonalityModule
+export default PersonalityModule;
