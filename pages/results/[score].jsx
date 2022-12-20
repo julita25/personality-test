35ce6f7c297683c31ef3d099b39166fb83485e0a
juli <julitati25@gmail.com>
2022-12-20 21:51:14 +0400

    add retake test button

--- pages/results/[score].jsx
+++ pages/results/[score].jsx
@@ -1,38 +1,43 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { Panel } from "rsuite";
+import { Button, Panel } from "rsuite";
 import { testResults } from "../../config/constants";
 
-
-const Results = () => {
+const ResultsPage = () => {
   const router = useRouter();
   const { score } = router.query;
   const result = score > 8 ? testResults[2] : testResults[1];
   const title = score > 8 ? "You are an extrovert" : "You are more of an introvert";
-  const img = score > 8 ? "/images/extrovert.png" : "/images/introvert.jpg"
-
-
+  const img = score > 8 ? "/images/extrovert.png" : "/images/introvert.jpg";
 
   return (
-    <div className="p-16">
-      <div className="p-10">
+    <div className="p-16 flex justify-center items-center">
+      <div className="p-10 space-y-10 flex flex-col justify-center items-start w-2/3">
         <Panel
           bordered
-          header={
+          header={(
             <div className="flex flex-col justify-center items-center space-y-2">
               <img src={img} alt="result picture" className="w-36" />
               <div>Your result</div>
             </div>
-          }
+          )}
         >
           <div className="space-y-5 flex flex-col jusitfy-center items-center">
             <div className="text-2xl">{title}</div>
             <div>{result}</div>
           </div>
         </Panel>
+        <Button
+          onClick={() => router.push("/")}
+          appearance="primary"
+          color="blue"
+          className="bg-blue-600"
+        >
+          Retake test
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Results;
+export default ResultsPage;
