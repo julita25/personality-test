f5c043f6390742bf6f16ee5427a38f7132919c95
juli <julitati25@gmail.com>
2022-12-20 18:37:53 +0400

    update style of main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { useState } from "react";
 import { Button } from "rsuite";
 import PersonalityModule from "../components/PersonalityModule/PersonalityModule";
@@ -5,13 +6,14 @@ import PersonalityModule from "../components/PersonalityModule/PersonalityModule
 const Main = () => {
   const [startTest, setStartTest] = useState(false)
   return (
-    <>
-      <div className="p-16">
-        <div className="p-10">
-          <div>Personality Test</div>
-          <div>
-            {
-              !startTest ?
+    <div className="h-full p-16">
+      <div className="p-10 flex flex-col justify-center items-center space-y-5">
+        <div className="text-5xl">Personality Test</div>
+        <div>
+          {
+            !startTest ?
+              <div className="space-y-5 flex flex-col justify-center">
+                <Image src="/images/cover.jpg" width={500} height={500} alt="cover" />
                 <Button
                   onClick={() => setStartTest(true)}
                   className="bg-blue-500"
@@ -19,13 +21,13 @@ const Main = () => {
                   appearance="primary">
                   Start test
                 </Button>
-                :
-                <PersonalityModule />
-            }
-          </div>
+              </div>
+              :
+              <PersonalityModule />
+          }
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
