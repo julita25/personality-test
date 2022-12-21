09bd6644efa7953a14524c52a894799b66409962
juli <julitati25@gmail.com>
2022-12-21 18:28:38 +0400

    add logic for handling error

--- components/PersonalityModule/PersonalityModule.jsx
+++ components/PersonalityModule/PersonalityModule.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Step from "../Step/Step";
 import TestSteps from "../TestSteps/TestSteps";
-import testQuestions from "../../data/questions.json";
 import { useRouter } from "next/router";
+import { Message } from "rsuite";
 
 const PersonalityModule = () => {
   const router = useRouter();
+  const [testQuestions, setTestQuestions] = useState([]);
+  const [error, setError] = useState();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [answers, setAnswers] = useState({});
+
+  const fetchTestQuestions = async () => {
+    const res = await fetch("/api/questions");
+    if (res?.ok) {
+      const response = await res.json()
+      setTestQuestions(response);
+      setError();
+    } else {
+      setError(new Error("Not possible to load test, try again"));
+    }
+  };
+
+  useEffect(() => {
+    fetchTestQuestions();
+  }, []);
 
   const onNextStep = (answer) => {
     setCurrentIndex(currentIndex + 1);
@@ -36,7 +53,12 @@ const PersonalityModule = () => {
 
   return (
     <>
-      <TestSteps steps={getSteps()} currentIndex={currentIndex} />
+      {Boolean(error) && (
+        <Message showIcon type="error" header={error.message} className="w-full" />
+      )}
+      {Boolean(testQuestions?.length) && (
+        <TestSteps steps={getSteps()} currentIndex={currentIndex} />
+      )}
     </>
   )
 };
